809719ec9f4c679e4a430697a48d9e5d4861c04b
maki <moritamaki0805@gmail.com>
2019-06-25 21:52:17 +0900

    make first step

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 'use strict';
 {
+    const word = 'starbacks coffee';
+
+
+    const target = document.getElementById('target');
+    const awesomeLabel = document.getElementById('awesome');
+    const missLabel = document.getElementById('miss');
+
     
+    let wordContent = [];
+    wordContent = word.split('').map(function(value) {
+         const span = document.createElement('span');
+         span.textContent = value;
+         target.appendChild(span);
+
+         return span;
+     });
+
+     console.log(wordContent);
+    
+    window.addEventListener('keyup', e => {
+            console.log(e.key);
+            if (e.key === wordContent[0].textContent) {
+                wordContent[0].classList.add('correct');
+                wordContent.shift();
+                console.log('awesome');
+            }
+    });
+   
 }

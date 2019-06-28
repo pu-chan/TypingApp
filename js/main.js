cb4ec951f730a4fd0e588338f1fa555edb3be37b
maki <moritamaki0805@gmail.com>
2019-06-28 19:14:29 +0900

    add timer

--- js/main.js
+++ js/main.js
@@ -35,16 +35,26 @@
         return words;
       }
 
+    // 定数, 変数
     const target = document.getElementById('target');
-    const awesomeLabel = document.getElementById('awesome');
-    const missLabel = document.getElementById('miss');
+    const order = document.getElementById('order');
+    const timer = document.getElementById('timer');
     
     let i = 0;
     let wordContent = [];
+    const timeLimit = 100 * 1000;
+    let startTime;
+    let isPlaying = false;
 
-    shuffle();
-    createWord();
+    order.textContent = '22 ';
+    timer.textContent = '100.00';
 
+
+    // ゲームスタート前
+    target.textContent = 'click to start';
+
+
+    // 単語の表示
     function createWord() {
             wordContent = words[i].split('').map(function(value) {
             
@@ -63,28 +73,84 @@
         });
     }
 
-    
-    window.addEventListener('keyup', e => {
-            if (e.key === wordContent[0].textContent) {
-                if(wordContent[0].textContent === ' ') {
-                    wordContent[0].textContent = '★';
-                    wordContent[0].classList.add('star');
-                    wordContent.shift();
-                }else {
-                    wordContent[0].classList.add('correct');
-                    wordContent.shift();
-                }
-            }
+    // タイマー
+    function updateTime() {
+       const timeLeft = startTime + timeLimit - Date.now(); 
+       timer.textContent = (timeLeft / 1000).toFixed(2); 
 
-            if(!wordContent.length) {
-                if(i < words.length - 1) {
-                    target.textContent = '';
-                    i++;
-                    createWord();
-                }else {
-                    window.alert('★Awesome★');
-                }
-                
+       const timeoutId = setTimeout(() => {
+            updateTime();
+       }, 10);
+
+       if(timeLeft < 0) {
+           isPlaying = false;
+           clearTimeout(timeoutId);
+           setTimeout(()=> {
+               alert('Finish★');
+            }, 100);
+            
+           timer.textContent = '0.00';
+           target.textContent = 'click to replay';
+       }
+    }
+
+    // タイピング
+    window.addEventListener('keyup', e => {
+        if(!isPlaying) {
+            return;
+         }
+        if (e.key === wordContent[0].textContent) {
+            if(wordContent[0].textContent === ' ') {
+                wordContent[0].textContent = '★';
+                wordContent[0].classList.add('star');
+                wordContent.shift();
+            }else {
+                wordContent[0].classList.add('correct');
+                wordContent.shift();
             }
+        }
+
+        if(!wordContent.length) {
+            if(i < words.length - 1) {
+                target.textContent = '';
+                i++;
+                createWord();
+            }else {
+                isPlaying = false;
+                clearTimeout(timeoutId);
+                setTimeout(()=> {
+                alert('Awesome');
+                }, 100);
+            
+                timer.textContent = '0.00';
+                target.textContent = 'click to replay';
+            }
+        }
+    });
+
+    // 画面をクリックしてスタート
+    document.addEventListener('click', () => {
+        if(isPlaying) {
+            return;
+        }
+        isPlaying = true;
+
+        target.textContent = '';
+        startTime = Date.now();
+        shuffle();
+        createWord();
+        updateTime();
+    });
+
+    // スタイル
+    const img = document.querySelector('img');
+    img.src = 'img/icon01.png';
+
+    img.addEventListener('mouseover', () => {
+        img.src = 'img/icon02.png';
+    });
+
+    img.addEventListener('mouseout', () => {
+        img.src = 'img/icon01.png';
     });
 }
